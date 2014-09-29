813f2e78ab9f815c9451a92d3a39991e50e4bb7b
henfredemars <marsrover7@gmail.com>
2014-09-29 13:20:19 -0400

    Added more tests

--- app/tests/user.server.model.test.js
+++ app/tests/user.server.model.test.js
@@ -41,12 +41,6 @@ describe('User Model Unit Tests:', function() {
 	});
 
 	describe('Method Save', function() {
-		it('should begin with no users', function(done) {
-			User.find({}, function(err, users) {
-				users.should.have.length(0);
-				done();
-			});
-		});
 
 		it('should be able to save without problems', function(done) {
 			user.save(done);
@@ -108,21 +102,137 @@ describe('User Model Unit Tests:', function() {
 			return;
 		});
 
-		it('should allow getting the status', function(done) {
-			var status = user.get('status');
-			assert.equal(status,user.status);
-			assert.notEqual(status,undefined);
+		it('should allow getting the invite_count', function(done) {
+			var invite_count = user.get('invite_count');
+			assert.equal(invite_count,user.invite_count);
+			assert.notEqual(invite_count,undefined);
+			done();
+			return;
+		});
+
+		it('should allow getting the attendee_count', function(done) {
+			var attendee_count = user.get('attendee_count');
+			assert.equal(attendee_count,user.attendee_count);
+			assert.notEqual(attendee_count,undefined);
+			done();
+			return;
+		});
+
+		it('should allow getting the rank', function(done) {
+			var rank = user.get('rank');
+			assert.equal(rank,user.rank);
+			assert.notEqual(rank,undefined);
+			done();
+			return;
+		});
+		
+		it('should allow getting the login_enable', function(done) {
+			var login_enable = user.get('login_enable');
+			assert.equal(login_enable,user.login_enable);
+			assert.notEqual(login_enable,undefined);
+			done();
+			return;
+		});
+
+		it('should allow getting the templates', function(done) {
+			var templates = user.get('templates');
+			assert.equal(templates,user.templates);
+			assert.notEqual(templates,undefined);
 			done();
 			return;
 		});
 
 		it('should be able to show an error when try to save without first name', function(done) {
+			var fName_old = user.fName;
 			user.fName = '';
 			return user.save(function(err) {
+				user.fName = fName_old;
 				should.exist(err);
 				done();
 			});
 		});
+
+		it('should be able to show an error when try to save without last name', function(done) {
+			var lName_old = user.lName;
+			user.lName = '';
+			return user.save(function(err) {
+				user.lName = lName_old;
+				should.exist(err);
+				done();
+			});
+		});
+
+		it('should be able to show an error when try to save without an email address', function(done) {
+			var email_old = user.email;
+			user.email = '';
+			return user.save(function(err) {
+				user.email = email_old;
+				should.exist(err);
+				done();
+			});
+		});
+
+		it('should be able to show an error when try to save without a password set', function(done) {
+			var password_old = user.password;
+			user.password = '';
+			return user.save(function(err) {
+				user.password = password_old;
+				should.exist(err);
+				done();
+			});
+		});
+
+		it('should be able to show an error when try to save without a role', function(done) {
+			var role_old = user.role;
+			user.role = '';
+			return user.save(function(err) {
+				user.role = role_old;
+				should.exist(err);
+				done();
+			});
+		});
+
+		it('should be able to show an error when try to save without a status associative array', function(done) {
+			var status_old = user.status;
+			user.status = undefined;
+			return user.save(function(err) {
+				user.status = status_old;
+				should.exist(err);
+				done();
+			});
+		});
+
+		it('should be able to show an error when try to save without an invite_count', function(done) {
+			var invite_count_old = user.invite_count;
+			user.invite_count = undefined;
+			return user.save(function(err) {
+				user.invite_count = invite_count_old;
+				should.exist(err);
+				done();
+			});
+		});
+
+		it('should be able to show an error when try to save without an attendee_count', function(done) {
+			var attendee_count_old = user.attendee_count;
+			user.attendee_count = undefined;
+			return user.save(function(err) {
+				user.attendee_count = attendee_count_old;
+				should.exist(err);
+				done();
+			});
+		});
+
+		it('should be able to show an error when try to save without a login_enable', function(done) {
+			var login_enable_old = user.login_enable;
+			user.login_enable = undefined;
+			return user.save(function(err) {
+				user.login_enable = login_enable_old;
+				should.exist(err);
+				done();
+			});
+		});
+
+
 	});
 
 	after(function(done) {
