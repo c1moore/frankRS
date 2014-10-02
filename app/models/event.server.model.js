8df184dee09dd74c2c78df2109eb751f7322800b
henfredemars <marsrover7@gmail.com>
2014-10-02 13:46:47 -0400

    Event schema first sorta-working version

--- app/models/event.server.model.js
+++ app/models/event.server.model.js
@@ -7,8 +7,12 @@ var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
 
 //Validation functions
-var ensureNotZeroLength = function(property) {
-	return (property.length!==0);
+var dateMustBeAtLeastToday = function(start_date) {
+	return (start_date >= Date.now);
+};
+
+var dateMustBeBeforeStart = function(end_date) {
+	return (this.start_date > end_Date);
 };
 
 /**
@@ -18,16 +22,27 @@ var EventSchema = new Schema({
 	name: {
 		type: String,
 		trim: true,
-		required: "A name is required",
-		default: '',
-		validate: [ensureNotZeroLength, 'Please fill in your first name']
+		required: "A name is required"
 	},
-	email: {
+	start_date: {
+		type: Date,
+		required: "A start date is required",
+		validate: [dateMustBeAtLeastToday, 'Date must be at least today if not later']
+	},
+	end_date: {
+		type: Date,
+		required: "An end date is required",
+		validate: [dateMustBeBeforeStart, 'End date must not exceed the start date']
+	},
+	location: {
 		type: String,
 		trim: true,
-		default: '',
-		validate: [ensureNotZeroLength, 'Please fill in your email'],
-		match: [/.+\@.+\..+/, 'Please fill a valid email address']
+		required: "The event must have a location"
+	},
+	schedule: {
+		type: String,
+		trim: true,
+		default: "No schedule specified"
 	}
 });
 
