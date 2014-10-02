a6d778d6bbe4b36129a69035675ff388a95c4be5
henfredemars <marsrover7@gmail.com>
2014-10-02 13:10:13 -0400

    Developing EventSchema

--- app/models/event.server.model.js
+++ app/models/event.server.model.js
@@ -7,22 +7,26 @@ var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
 
 //Validation functions
+var ensureNotZeroLength = function(property) {
+	return (property.length!==0);
+};
 
 /**
  * Event Schema
  */
 var EventSchema = new Schema({
-	fName: {
+	name: {
 		type: String,
 		trim: true,
+		required: "A name is required",
 		default: '',
-		validate: [validateLocalStrategyProperty, 'Please fill in your first name']
+		validate: [ensureNotZeroLength, 'Please fill in your first name']
 	},
 	email: {
 		type: String,
 		trim: true,
 		default: '',
-		validate: [validateLocalStrategyProperty, 'Please fill in your email'],
+		validate: [ensureNotZeroLength, 'Please fill in your email'],
 		match: [/.+\@.+\..+/, 'Please fill a valid email address']
 	}
 });
