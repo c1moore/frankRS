56ead406b560af533ae1f7a78faebc1343d59875
henfredemars <marsrover7@gmail.com>
2014-10-05 14:59:31 -0400

    Things were done

--- app/tests/events.server.model.test.js
+++ app/tests/events.server.model.test.js
@@ -22,21 +22,22 @@ describe('Event Model Unit Tests',function() {
 		beforeEach(function(done){
 			event1 = new events({
 				contents: {
-				name:  "testing123",
-				start_date: "10.30.2014",
-				end_date: "10.31.2014",
-				location: "UF",
+					name:  "testing123",
+					start_date: "10.30.2014",
+					end_date: "10.31.2014",
+					location: "UF",
+				},
 				schedule: "www.google.com"
-			}});
+			});
 			event2 = new events({
 				contents: {
-				name:  "testing123",
-				start_date: "10.30.2014",
-				end_date: "10.31.2014",
-				location: "UF",
-				schedule: "www.google.com"
-			}});
-
+					name:  "testing123",
+					start_date: "10.30.2014",
+					end_date: "10.31.2014",
+					location: "UF",
+				},
+			schedule: "www.google.com"
+			});
 			done();
 		});
 
@@ -98,7 +99,7 @@ describe('Event Model Unit Tests',function() {
 			var ename_old = event1.contents.name;
 			event1.contents.name= '';
 			return event1.save(function(err) {
-				event1.name = ename_old;
+				event1.contents.name = ename_old;
 				should.exist(err);
 				done();
 			});
