50f30a449f2dc8e50a87e95d742d9267f8e5de1b
dalton <daltonverhagen@gmail.com>
2014-10-09 12:47:04 -0400

    set ng-repeat for buttons with test data

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,5 +5,15 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+
+		// Temporary data for buttons
+		$scope.data = {
+			buttons: [
+				{name:"Button 1", description:"Description 1"},
+				{name:"Button 2", description:"Description 2"},
+				{name:"Button 3", description:"Description 3"},
+				{name:"Button 4", description:"Description 4"}
+			]
+		};
 	}
 ]);
