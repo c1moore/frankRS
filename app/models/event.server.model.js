aaf5da71d6ee0adda6db8a0f12b80ef868e637a7
dtp130 <dtp130@gmail.com>
2014-10-09 15:37:04 -0400

    Started fixing Date objects in Events

--- app/models/event.server.model.js
+++ app/models/event.server.model.js
@@ -8,12 +8,12 @@ var mongoose = require('mongoose'),
 
 //Validation functions
 var dateMustBeAtLeastToday = function(start_date) {
-	return (start_date >= Date.getTime());
+	return (Date.parse(start_date) > new Date().getTime());
 };
-
+/*
 var dateMustBeBeforeStart = function(end_date) {
-	return (this.start_date > end_date);
-};
+	return (Date(start_date).getTime() < Date(end_date).getTime());
+};*/
 
 var contentsValidate = function(property) {
 	return ((property.name && property.name.length) && (property.start_date && property.start_date.length) && (property.end_date && property.end_date.length) && (property.location && property.location.length));
@@ -33,12 +33,12 @@ var EventSchema = new Schema({
 				//validate: [contentsValidate, 'A name is required.']
 			},
 			start_date: {
-				type: Date,
+				type: String,
 				validate: [dateMustBeAtLeastToday, 'Date must be at least today if not later'] //, contentsValidate, 'Start date is required.']
 			},
 			end_date: {
-				type: Date,
-				validate: [dateMustBeBeforeStart, 'End date must not exceed the start date'] //, contentsValidate, 'End date is required.']
+				type: String
+				//validate: [dateMustBeBeforeStart, 'End date must not exceed the start date'] //, contentsValidate, 'End date is required.']
 			},
 			location: {
 				type: String,
@@ -54,6 +54,26 @@ var EventSchema = new Schema({
 		trim: true,
 		default: 'No schedule specified'
 	}
+});
+
+EventSchema.pre('validate', function(next) {
+	if(Date.parse(this.contents.start_date) > Date.parse(this.contents.end_date)){
+		this.invalidate('end_date','end date must be after start date');
+	}	
+	if(Date.parse(this.contents.end_date) < new Date().getTime()){
+		this.invalidate('end_date','end date must not be in the past');
+	}
+	next();
+});
+
+EventSchema.pre('save', function(next) {
+	if (this.contents.start_date) {
+		this.contents.start_date=this.contents.start_date.toString();
+	}
+	if (this.contents.end_date) {
+		this.contents.end_date=this.contents.end_date.toString();
+	}
+	next();
 });
 
 mongoose.model('Event', EventSchema);
