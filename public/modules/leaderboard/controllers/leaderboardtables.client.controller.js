6c5d807a1934514a100ddfc3cdd758c946e3b94d
daltonv <daltonverhagen@gmail.com>
2014-11-02 20:46:21 -0500

    added tables for attending and invited leaderboards

--- public/modules/leaderboard/controllers/leaderboardtables.client.controller.js
+++ public/modules/leaderboard/controllers/leaderboardtables.client.controller.js
@@ -6,14 +6,16 @@ angular.module('leaderboard').controller('LeaderboardTablesCtrl', ['$scope', 'Au
 			error: null
 		};
 
-		$http.get('/modules/leaderboard/tests/MOCK_DATA.json').success(function(data) {
-			$scope.data.users = data;
+		var mainApi = '/leaderboard/maintable';
+		var testApi = '/modules/leaderboard/tests/MOCK_DATA.json';
 
-			$scope.tableParams = new ngTableParams({
+		$http.get(testApi).success(function(data) {
+
+			$scope.mainTableParams = new ngTableParams({
 	        	page: 1,            // show first page
 	        	count: 10,           // count per page
 	        	filter: {
-	        		displayName:'l'	//set the initial filter to nothing for name
+	        		displayName:''	//set the initial filter to nothing for name
 	        	},
 	        	sorting: {
 	        		rank:'asc'		// set the initial sorting to be rank asc
@@ -22,11 +24,40 @@ angular.module('leaderboard').controller('LeaderboardTablesCtrl', ['$scope', 'Au
 	        	total: $scope.data.users.length, // length of data
 	        	getData: function($defer, params) {
 	            	var filteredData = params.filter() ?
-	            		$filter('filter')($scope.data.users, params.filter()) :
-	            		$scope.data.users;
+	            		$filter('filter')(data, params.filter()) :
+	            		data;
 	            	var orderedData = params.sorting() ? 
 	            		$filter('orderBy')(filteredData, params.orderBy()) : 
-	            		$scope.data.users;
+	            		data;
+
+	            	params.total(orderedData.length); //set total recalculation for paganation
+	            	$defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+	        	}
+    		});
+		}).error(function(error){
+			$scope.data.error = error;
+		});
+
+		$http.get('/modules/leaderboard/tests/MOCK_ATTENDEE_DATA.json').success(function(data) {
+
+			$scope.attendingTableParams = new ngTableParams({
+	        	page: 1,            // show first page
+	        	count: 10,           // count per page
+	        	filter: {
+	        		displayName:''	//set the initial filter to nothing for name
+	        	},
+	        	sorting: {
+	        		displayName:'asc'		// set the initial sorting to be displayName asc
+	        	}
+	    		}, {
+	        	total: $scope.data.users.length, // length of data
+	        	getData: function($defer, params) {
+	            	var filteredData = params.filter() ?
+	            		$filter('filter')(data, params.filter()) :
+	            		data;
+	            	var orderedData = params.sorting() ? 
+	            		$filter('orderBy')(filteredData, params.orderBy()) : 
+	            		data;
 
 	            	params.total(orderedData.length); //set total recalculation for paganation
 	            	$defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
