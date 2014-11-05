5a03ef32cb23dd65dce270906afe201c81e00ffb
henfredemars <marsrover7@gmail.com>
2014-11-04 21:03:58 -0500

    Completed Event class, added more utilities and updated files

--- populator/Event.py
+++ populator/Event.py
@@ -3,6 +3,9 @@
 from Util import randomString
 from Util import randomTimeInMS
 from Util import DATABASE
+from Util import getPymongoDB
+
+import inspect
 
 class Event:
 
@@ -20,4 +23,20 @@ class Event:
     return (hasattr(self,name) and hasattr(self,schedule) and hasattr(self,location) and
 		hasattr(self,start_date) and hasattr(self,end_date))
 
-  
+  def save(self):
+    members = inspect.getMembers(self)
+    names = [name for name, val in members if not name.contains('_') and
+		not inspect.isfunction(val) and not inspect.isclass(val) and
+		not inspect.ismodule(val) and not inspect.ismethod(val) and
+		not inspect.isbuiltin(val)]
+    if not self.valid():
+      raise RuntimeError("Event: Object is not ready")
+    db = getPymongoDB()
+    dic = dict()
+    for name in names:
+      dic[name] = self.__dict__[name]
+    Events = db.events
+    self.id = Events.insert(dic)
+    print("Event->insert: {}".format(str(dic)))
+    return self.id
+
--- populator/Util.py
+++ populator/Util.py
@@ -1,5 +1,7 @@
 #General utility functions that dont belong in a class
 
+import pymongo
+
 import random
 import string
 import os
@@ -8,6 +10,7 @@ TWENTY_YEARS = 631138519494
 THIRTY_YEARS = 946707779241
 MONGO_PATH = '/usr/bin/mongo'
 DATABASE = "frank-recruiter-system-dev"
+PymongoDB = None
 
 def randomString(min,max):
   return ''.join(random.choice(string.ascii_uppercase+string.ascii_lowercase+
@@ -19,3 +22,11 @@ def randomTimeInMS(mininum=TWENTY_YEARS):
 def resetMongo():
   os.system('{} --eval "db.dropDatabase();" frank-recruiter-system-test'.format(MONGO_PATH))
   os.system('{} --eval "db.dropDatabase();" {}'.format(MONGO_PATH,DATABASE))
+
+def getPymongoDB():
+  if not PymongoDB:
+    client = pymongo.MongoClient('mongod://localhost:27017/')
+    PymongoDB = client[DATABASE]
+  return PymongoDB
+
+
