14c8c6fcbe141efeb90e52b4815cc90f7a4a50b1
henfredemars <marsrover7@gmail.com>
2014-11-06 23:23:11 -0500

    Embedded in main to allow importing

--- populator/tests.py
+++ populator/tests.py
@@ -11,134 +11,139 @@ from Util import resetMongo
 #	that the results are relatively sane and at least runnable from python's
 #	point of view
 
-print("Attempting to reset MongoDB")
-resetMongo()
+def main():
 
-print("\nTesting dependencies\n")
+  print("Attempting to reset MongoDB")
+  resetMongo()
 
-try:
-  import pymongo
-except Exception as e:
-  print("--FAILED to import pymongo. Dependencies not satisfied?")
-  raise e
-else:
-  print("--SUCCESS when importing pymongo")
+  print("\nTesting dependencies\n")
 
-print("\nTesting User object functionality\n")
+  try:
+    import pymongo
+  except Exception as e:
+    print("--FAILED to import pymongo. Dependencies not satisfied?")
+    raise e
+  else:
+    print("--SUCCESS when importing pymongo")
 
-try:
-  user = User()
-except Exception as e:
-  print("--FAILED to instantiate new user")
-  raise e
-else:
-  print("--SUCCESS when instantiating a new user")
+  print("\nTesting User object functionality\n")
 
-try:
-  user.randomize()
-except Exception as e:
-  print("--FAILED to randomize a User object")
-  raise e
-else:
-  print("--SUCCESS when randomizing a User object")
+  try:
+    user = User()
+  except Exception as e:
+    print("--FAILED to instantiate new user")
+    raise e
+  else:
+    print("--SUCCESS when instantiating a new user")
 
-try:
-  assert(user.valid())
-except Exception as e:
-  print("--FAILED to verify validity of user object")
-  raise e
-else:
-  print("--SUCCESS when verifying validity of user object")
+  try:
+    user.randomize()
+  except Exception as e:
+    print("--FAILED to randomize a User object")
+    raise e
+  else:
+    print("--SUCCESS when randomizing a User object")
 
-try:
-  user.save() #Eek!
-except Exception as e:
-  print("--FAILED to save a user object")
-  raise e
-else:
-  print("--SUCCESS when saving a user object")
+  try:
+    assert(user.valid())
+  except Exception as e:
+    print("--FAILED to verify validity of user object")
+    raise e
+  else:
+    print("--SUCCESS when verifying validity of user object")
 
-print("\nTesting Event object functionality\n")
+  try:
+    user.save() #Eek!
+  except Exception as e:
+    print("--FAILED to save a user object")
+    raise e
+  else:
+    print("--SUCCESS when saving a user object")
 
-try:
-  event = Event()
-except Exception as e:
-  print("--FAILED to instantiate new event")
-  raise e
-else:
-  print("--SUCCESS in instantiating new event")
+  print("\nTesting Event object functionality\n")
 
-try:
-  event.randomize()
-except Exception as e:
-  print("--FAILED to randomize event object")
-  raise e
-else:
-  print("--SUCCESS in randomizing event object")
+  try:
+    event = Event()
+  except Exception as e:
+    print("--FAILED to instantiate new event")
+    raise e
+  else:
+    print("--SUCCESS in instantiating new event")
 
-try:
-  event.save()
-except Exception as e:
-  print("--FAILED to save event object")
-  raise e
-else:
-  print("--SUCCESS in saving event object")
+  try:
+    event.randomize()
+  except Exception as e:
+    print("--FAILED to randomize event object")
+    raise e
+  else:
+    print("--SUCCESS in randomizing event object")
 
-print("\nTesting Candidate object functionality\n")
+  try:
+    event.save()
+  except Exception as e:
+    print("--FAILED to save event object")
+    raise e
+  else:
+    print("--SUCCESS in saving event object")
 
-try:
-  candidate = Candidate()
-except Exception as e:
-  print("--FAILED to instantiate new candidate object")
-  raise e
-else:
-  print("--SUCCESS in instantiating new candidate object")
+  print("\nTesting Candidate object functionality\n")
 
-try:
-  candidate.randomize()
-except Exception as e:
-  print("--FAILED to randomize candidate object")
-  raise e
-else:
-  print("--SUCCESS in randomizing candidate object")
+  try:
+    candidate = Candidate()
+  except Exception as e:
+    print("--FAILED to instantiate new candidate object")
+    raise e
+  else:
+    print("--SUCCESS in instantiating new candidate object")
 
-try:
-  candidate.save()
-except Exception as e:
-  print("--FAILED to save candidate object")
-  raise e
-else:
-  print("--SUCCESS in saving candidate object")
+  try:
+    candidate.randomize()
+  except Exception as e:
+    print("--FAILED to randomize candidate object")
+    raise e
+  else:
+    print("--SUCCESS in randomizing candidate object")
 
-try:
-  candidate.fName = "SomethingElse"
-  candidate.save()
-except Exception as e:
-  print("--FAILED to update candidate in database")
-  raise e
-else:
-  print("--SUCCESS in updating candidate in database")
+  try:
+    candidate.save()
+  except Exception as e:
+    print("--FAILED to save candidate object")
+    raise e
+  else:
+    print("--SUCCESS in saving candidate object")
 
-print("\nIntegration testing\n")
+  try:
+    candidate.fName = "SomethingElse"
+    candidate.save()
+  except Exception as e:
+    print("--FAILED to update candidate in database")
+    raise e
+  else:
+    print("--SUCCESS in updating candidate in database")
 
-try:
-  user.roles.append('recruiter')
-  user.save()
-  user2 = User()
-  user2.randomize()
-  user.invite(user2,event._id)
-except Exception as e:
-  print("--FAILED to invite a user")
-  raise e
-else:
-  print("--SUCCESS in in inviting a user")
+  print("\nIntegration testing\n")
 
-try:
-  user2.decide(event._id,True,False,user)
-except Exception as e:
-  print("--FAILED user accept an invitation")
-  raise e
-else:
-  print("--SUCCESS user accept an invitation")
+  try:
+    user.roles.append('recruiter')
+    user.save()
+    user2 = User()
+    user2.randomize()
+    user.invite(user2,event._id)
+  except Exception as e:
+    print("--FAILED to invite a user")
+    raise e
+  else:
+    print("--SUCCESS in in inviting a user")
+
+  try:
+    user2.decide(event._id,True,False,user)
+  except Exception as e:
+    print("--FAILED user accept an invitation")
+    raise e
+  else:
+    print("--SUCCESS user accept an invitation")
+
+if __name__=='__main__':
+  main()
 
 
