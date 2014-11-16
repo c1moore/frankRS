06e6b8e1dcf9ca605ce2e40337e46591b53439f9
henfredemars <marsrover7@gmail.com>
2014-11-16 12:25:43 -0500

    Added new schema, deleted extra files, added more intuitive naming conventions, removal of the unused attendee schema from the populator and updates tests to reflect the changes

---
+++ app/models/comment.server.model.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema,
+	idvalidator = require('mongoose-id-validator');
+
+//Validation functions
+
+var validateRequired = function(property) {
+	return (property && property.length);
+};
+
+
+/**
+ * Event Schema
+ */
+var CommentSchema = new Schema({
+	user_id: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+		validate: [validateRequired, 'user_id is required.']
+	},
+	event_id: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Event',
+		validate: [validateRequired, 'event_id is required.']
+	},
+	comment: {
+		type: String,
+		validate: [validateRequired, 'Comment body is required.']
+	},
+	stream: {
+		type: String,
+		enum: ['recruiter', 'social'],
+		validate: [validateRequired, 'A comment stream type is required.']
+	}
+});
+
+//ID validator
+CommentSchema.plugin(idvalidator);
+
+mongoose.model('Comment', CommentSchema);
