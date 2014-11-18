925986b7dbacae7ff60542e6bb165a21ccd4272d
Calvin Moore <c1moore@ufl.edu>
2014-11-18 12:07:51 -0500

    Fixed the eventselector and added a few methods that will be necessary when recruiters visit pages where they should only see information about the events for which they are recruiting.

--- public/modules/core/services/event-selector.client.service.js
+++ public/modules/core/services/event-selector.client.service.js
@@ -1,26 +1,42 @@
 'use strict';
 
 //Menu service used for managing  menus
-angular.module('core').service('Event-Selector', ['$http',
+angular.module('core').service('eventSelector', ['$http',
 
 	function($http) {
-		var events = [];
-		var selectedEvent = "Select Event";
-		var disabled = false;
+		var thisService = this;
+		this.events = [];
+		this.selectedEvent = "Select Event";
+		this.disabled = false;
+		this.numRecruiting = 0;
+		this.postEventId = null;
+		this.nresDisabled = false;
 
 		$http.get('/users/events').success(function(data) {
-			events = data;
+			thisService.events = data.status;
+			for(var i=0; i<thisService.events.length; i++) {
+				if(thisService.events[i].recruiter)
+					thisService.numRecruiting++;
+			}
 		}).error(function(error, status) {
 			if(status === 401) {
-				selectedEvent = "Error";
-				disabled = !disabled;
+				thisService.selectedEvent = "Error";
+				thisService.disabled = !thisService.disabled;
 			}
 			console.log(error);
 		});
 
-		var changeEvent = function(event) {
-			selectedEvent = event.name;
-			postEventId = event._id;
+		this.changeEvent = function(event) {
+			thisService.selectedEvent = event.event_id.name;
+			thisService.postEventId = event.event_id._id;
 		};
+
+		this.showDivider = function() {
+			return ((thisService.numRecruiting > 0) && (thisService.events.length > thisService.numRecruiting));
+		};
+
+		this.toggleDisabledEvents = function() {
+			thisService.nresDisabled = !thisService.nresDisabled;
+		}
 	}
 ]);
