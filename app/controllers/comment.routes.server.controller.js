3a51abee4a79a79fb82f82f0fd2dcd29233d2902
henfredemars <marsrover7@gmail.com>
2014-11-21 14:37:51 -0500

    Progress on the controllers and adding more routes, tests to be added, more work to be done!

--- app/controllers/comment.routes.server.controller.js
+++ app/controllers/comment.routes.server.controller.js
@@ -9,6 +9,8 @@
 
 //Okay, but does this mean that a user can learn that an event exists that they aren't supposed to know about? No, there should be a field in the comments schema specifying which event this comment was created for.  You should require that event_id and only return comments for that event
 
+//CONTINUE HERE TODO COMMENT ROUTES CONTROLLERS
+
 var errorHandler = require('./errors'),
 	mongoose = require('mongoose'),
 	Comment = mongoose.model('Comment'),
@@ -18,7 +20,19 @@ var errorHandler = require('./errors'),
 var canViewComment = function(user,hasAuthorization,comment) {
 	if (comment.stream=='social') return true;
 	if (hasAuthorization(user,['admin'])) return true;
-	if (hasAuthorization(user,['recruiter']) && comment.stream='recruiter') return true;
+	if (hasAuthorization(user,['recruiter']) && comment.stream='recruiter' && canViewEvent(
+		user,comment.event_id,hasAuthorization)) return true;
+	return false;
+};
+
+var canViewEvent = function(user,eventID,hasAuthorization) {
+	var statusArray = user.status;
+	for (var i = 0; i<statusArray.length;i++) {
+		if(statusArray[i].event_id==eventID) {
+			return true;
+		}
+	}
+	if (hasAuthorization(user,['admin'])) return true;
 	return false;
 };
 
@@ -29,10 +43,8 @@ exports.getCommentObj = function(req, res) {
 	}
 	var id = req.body.comment_id;
 	var query = Comment.findOne({_id: id});
-	var user;
 	//Retrieve the comment
 	query.exec(function(err,result) {
-		user = result;
 		if (err) {res.status(400).send(err);return;}
 		else if (!user) {res.status(400).json({message: "No comment found!"});return;}
 		else if (!canViewComment(req.user,req.hasAuthorization,result)) {
@@ -50,10 +62,8 @@ exports.getSocialCommentsForEvent = function(req, res) {
 	}
 	var id = req.body.event_id;
 	var query = Comment.find({event_id: id,stream: 'social'});
-	var user;
 	//Retrieve the comments
 	query.exec(function(err,result) {
-		user = result;
 		if (err) {res.status(400).send(err);return;}
 		else if (!user) {res.status(400).json({message: "No comments found!"});
 		} else {
@@ -72,13 +82,49 @@ exports.getRecruiterCommentsForEvent = function(req, res) {
 	var user;
 	//Retrieve the comments
 	query.exec(function(err,result) {
-		user = result;
 		if (err) {res.status(400).send(err);return;}
 		else if (!user) {res.status(400).json({message: "No comments found!"});
 		} else if (!req.hasAuthorization(user,['recruiter'])) {
 			res.status(401).json({message: "You are not a recruiter"});
+		} else if (!canViewEvent(user,result.event_id,req.hasAuthorization)) {
+			res.status(401).json({message: "You are not authorized to view comments of this event"});
 		} else {
 			res.status(200).json(result);
+		}
+	});
+};
+
+exports.postCommentSocial = function(req, res) {
+	if (!req.isAuthenticated()) { //Check if the user is authenticated
+		res.status(401).json({message: "You are not logged in"});
+		return;
+	}
+	var comment = req.body.comment;
+	var event_id = req.body.event_id;
+	var query = Comment.findOne({_id: id});
+	var user = req.user;
+	Comment.insert({user_id: user._id,event_id: event_id,comment:comment,stream:'social'});
+};
+
+exports.postComment
+
+exports.delete = function(req, res) {
+	if (!req.isAuthenticated()) { //Check if the user is authenticated
+		res.status(401).json({message: "You are not logged in"});
+		return;
+	}
+	var id = req.body.comment_id;
+	var query = Comment.findOne({_id: id});
+	//Retrieve the comments
+	query.exec(function(err,result) {
+		user = result;
+		if (err) {res.status(400).send(err);return;}
+		else if (!user) {res.status(400).json({message: "No comment found!"});
+		} else if (req.user._id!=result.user_id && !req.hasAuthorization(req.user,['admin'])) {
+			res.status(401).json({message: "You must be the comment author or admin to delete"});
+		} else {
+			result.remove();
+			res.status(200).json({message: "Comment removed"});
 		}
 	});
 };
