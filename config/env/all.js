268176b5b66d84f70d0e82834eeae09aae8d37b2
Calvin Moore <c1moore@ufl.edu>
2014-11-22 16:25:54 -0500

    Added jQuery as a dependency.

--- config/env/all.js
+++ config/env/all.js
@@ -30,7 +30,8 @@ module.exports = {
 				'public/lib/angular-ui-utils/ui-utils.js',
 				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
 				'public/lib/ng-table/ng-table.js',
-				'public/lib/lodash/dist/lodash.js'
+				'public/lib/lodash/dist/lodash.js',
+				'public/lib/jquery-ui/jquery-ui.min.js'
 			]
 		},
 		css: [
