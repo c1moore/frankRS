a93f2978cd075b5f2946f227f830630eb979bc77
dalton <daltonverhagen@gmail.com>
2014-11-24 12:21:50 -0500

    added angular-multi-select directive to the project

--- public/config.js
+++ public/config.js
@@ -4,7 +4,7 @@
 var ApplicationConfiguration = (function() {
 	// Init module configuration options
 	var applicationModuleName = 'frank-recruiter-system';
-	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ui.bootstrap', 'ui.utils', 'ngTable'];
+	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ui.bootstrap', 'ui.utils', 'ngTable', 'multi-select'];
 
 	// Add a new vertical module
 	var registerModule = function(moduleName, dependencies) {
