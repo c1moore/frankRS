ddc1c8b8ca1ca4df9f8a4349bc687061a18c7fc3
Calvin Moore <c1moore@ufl.edu>
2014-11-25 17:16:36 -0500

    Added directive and controller for comments section on leaderboard.

---
+++ public/modules/core/directives/comment.client.directive.js
@@ -0,0 +1,89 @@
+'use strict';
+
+angular.module('core').directive('comment', [
+	function() {
+		var commentDefinition = {
+			template: '<div class="frank-comment" ng-transclude></div>',
+			restrict: 'E',
+			replace : true,
+			transclude : true,
+			//link: function postLink(scope, element, attrs) {
+			//}
+		};
+
+		return commentDefinition;
+	}
+]);
+
+angular.module('core').directive('commentHeader', [
+	function() {
+		var commentHeaderDefinition = {
+			restrict : 'E',
+			scope : {
+				author : '@authorName',
+				time : '@postTime',
+				image : '@authorImage',
+				removable : '@'
+			},
+			require : '^comment',
+			replace : true,
+			template : "<div class='frank-comment-header'>" +
+							"<div ng-if='image' class='frank-comment-image-container'>" +
+								"<img src='{{image}}' class='frank-comment-image' />" +
+							"</div>" +
+							"<div class='frank-comment-author'>" +
+								"<span>{{author}}</span>" +
+							"</div>" +
+							"<div class='frank-comment-time'>" +
+								"<span>{{time}}</span>" +
+							"</div>" +
+							"<div ng-if='removable' class='frank-comment-remove'>" +
+								"<a href='#' class='frank-comment-remove-icon'><i class='fa fa-remove text-danger'></i></a>"
+							"</div>" +
+						"</div>"
+		};
+
+		return commentHeaderDefinition;
+	}
+]);
+
+angular.module('core').directive('commentBody', [
+	function() {
+		var commentBodyDefinition = {
+			restrict : 'E',
+			transclude : true,
+			replace : true,
+			require : '^comment',
+			template : "<div class='frank-comment-body'>" +
+							"<div class='frank-comment-message'>" +
+								"<span ng-transclude></span>" +
+							"</div>" +
+						"</div>"
+		};
+
+		return commentBodyDefinition;
+	}
+]);
+
+angular.module('core').directive('commentFooter', [
+	function() {
+		var commentFooterDefinition = {
+			restrict : 'E',
+			transclude : true,
+			replace : true,
+			scope : {
+				interests : '='
+			},
+			require : '^comment',
+			template : "<div class='frank-comment-footer'>" +
+							"<div class='frank-comment-interests'>" +
+								"<div ng-repeat='interest in interests'>" +
+									"<img src='interest.url' alt='interest.text' />" +
+								"</div>" +
+							"</div>" +
+						"</div>"
+		};
+
+		return commentFooterDefinition;
+	}
+]);
---
+++ public/modules/leaderboard/controllers/comments.client.controller.js
@@ -0,0 +1,45 @@
+'use strict';
+
+angular.module('leaderboard').controller('commentsCtrl', ['$scope', 'Authentication', '$location', 'eventSelector', '$http', '$window', '$modal', 'cacheService', '$interval',
+	function($scope, Authentication, $location, eventSelector, $http, $window, $modal, cacheService, $interval) {
+		$scope.authentication = Authentication;
+
+		$scope.removable() = function(user_id) {
+			if(_.intersection($scope.authentication.user.roles, ['admin']).length === 1)
+				return true;
+			else {
+				if(user_id.toString() === $scope.authentication.user._id.toString())
+					return true;
+			}
+
+			return false;
+		};
+
+		/**
+		* Get all the comments from the database for recruiters for
+		* this event.
+		*/
+		var getComments = function() {
+			$http.post('/comments/getRecruiterCommentsForEvent', {event_id : eventSelector.postEventId}).success(function(response) {
+				$scope.comments = response;
+			}).error(function(response, status) {
+				if(status === 401) {
+					if(response.message === "User is not logged in.") {
+						$location.path('/signin');
+					} else {
+						$location.path('/');
+					}
+				} else if(status === 400) {
+					$scope.comments.error = "Error retrieving comments.  Try refreshing the page.";
+				}
+			});
+		};
+
+		getComments();
+
+		//Update comments every 2 minutes.
+		$interval(getComments(), 120000);
+
+
+	}
+]);
