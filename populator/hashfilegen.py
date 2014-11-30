8d7e3d834cd484aabca01fb6a18ebc82ae768b51
James B <marsrover7@gmail.com>
2014-11-30 01:43:04 -0500

    Added record of how I obtained working salt+hash=>password for logging-in generated users

---
+++ populator/hashfilegen.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+#Script that was used to create hashfile.dat, containing a salt and password
+#You must first create a single valid user in the database for me to locate
+#Assumes that the plaintext password was "password" in User.py
+
+from Util import getPymongoDB
+
+import pickle
+
+def main():
+  db = getPymongoDB()
+  user = list(db.users.find())[0]
+  salt = user['salt']
+  password = user['password'] #Hashed
+  with open('hashfile.dat','w') as fd:
+    fd.write(salt)
+    fd.write(password)
+
+if __name__=='__main__':
+  print("Cowardly refusing to run because I'm not confident that you know what you're doing.")
+
+
