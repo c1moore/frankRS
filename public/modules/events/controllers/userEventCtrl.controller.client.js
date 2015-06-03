57c4cfacc5ccf9229d876bf684a7327bb3aab567
Calvin Moore <c1moore@ufl.edu>
2015-06-03 17:43:06 -0400

    Began adding form for those requesting to be a recruiter.

--- public/modules/events/controllers/userEventCtrl.controller.client.js
+++ public/modules/events/controllers/userEventCtrl.controller.client.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('events').controller('userEventCtrl', ['$scope', 'ngTableParams', '$http', 'eventSelector', '$filter', 'dialogs', 'Authentication', '$timeout', '$window',
-	function($scope, ngTableParams, $http, eventSelector, $filter, dialogs, Authentication, $timeout, $window) {
+angular.module('events').controller('userEventCtrl', ['$scope', 'ngTableParams', '$http', 'eventSelector', '$filter', 'dialogs', 'Authentication', '$timeout', '$window', '$modal',
+	function($scope, ngTableParams, $http, eventSelector, $filter, dialogs, Authentication, $timeout, $window, $modal) {
 		$scope.user = Authentication;
 		
 		$scope.dpOpen = false;
@@ -36,7 +36,7 @@ angular.module('events').controller('userEventCtrl', ['$scope', 'ngTableParams',
 
 	  	$scope.tableParams = new ngTableParams({
         	page: 1,
-			count: 5,
+			count: 10,
 			filter: {
 			        name:''
 			  },
@@ -64,7 +64,7 @@ angular.module('events').controller('userEventCtrl', ['$scope', 'ngTableParams',
 			});
 		});
 
-		$scope.launch = function(event) {
+		/*$scope.launch = function(event) {
 			dlg = dialogs.confirm("Confirmation Vital", "Are you absolutely sure you want to go through the rigorous tests we put forth to become a recruiter for " + event.name + "? <br /><br />(You obviously have the right stuff if you have access to this page.)", {windowClass : "frank-recruiter-signup-modal"});
 			dlg.result.then(function(btn){
 				$http.post("candidate/setCandidate", {event_id:event._id}).success(function() {
@@ -73,7 +73,33 @@ angular.module('events').controller('userEventCtrl', ['$scope', 'ngTableParams',
 					$window.alert("There was an error submitting your request.  Please try again later.");
 				});
 			});
+		};*/
+
+		$scope.launch = function(event) {
+			var modalInstance = $modal.open({
+				templateUrl: 	"modules/events/views/recruiter-form.client.view.html",
+				controller: 	"FormModalCtrl",
+				backdrop: 		'static',
+				keyboard: 		false,
+				resolve: 		{
+					event: 	function() {
+						return event;
+					}
+				}
+			});
+
+			/**
+			* The modal instance should return with either a truthy value.  If this value evaluates to
+			* true, the table is updated.  Otherwise, the table will not be updated.
+			*/
+			modalInstance.result.then(function(answers) {
+				if(answers) {
+					getEvents();
+				}
+			});
 		};
+
+		$timeout(function() {$scope.launch($scope.events[0]);}, 5000);
 
 		/**
 		* Adopted from post made on stackoverflow.com by disfated.  Original post here:
@@ -81,6 +107,110 @@ angular.module('events').controller('userEventCtrl', ['$scope', 'ngTableParams',
 		*/
 		$scope.capitalize = function(string, lower) {
 			return (lower ? string.toLowerCase() : string).replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+		};
+	}
+]);
+
+angular.module("events").controller("FormModalCtrl", ["$scope", "$modalInstance", "event", "usSpinnerService", "$http",
+	function($scope, $modalInstance, event, usSpinnerService, $http) {
+		$scope.event = event;
+		$scope.answers = {};
+		$scope.answers.reason = "";
+		$scope.answers.connection = "";
+
+		/**
+		* 
+		*/
+		$http.post('/candidate/me').success(function(candidate, status) {
+			if(status === 200) {
+				if(candidate.note) {
+					var startRegex = /\*\*\*\*\*\*\*\*\*\*/;
+					var endRegex = /\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*/;
+					var reasonRegex = /\*\*\*Reason:/;
+					var connRegex = /\*\*\*Connections:/;
+
+					if(candidate.note.search(startRegex) !== -1) {
+						var rindex = candidate.note.search(reasonRegex);
+						if(rindex !== -1) {
+							rindex += 11;		//Go to the start of the reason answer.
+						}
+
+						var cindex = candidate.note.search(connRegex);
+						var rend;				//End of the reason answer.
+						if(cindex !== -1) {
+							rend = cindex - 2;
+							cindex += 16;		//Go to the start of the connection answer.
+						}
+
+						var cend = candidate.note.search(endRegex);
+
+						if(rindex !== -1 && cindex > rindex && cend > cindex) {
+							$scope.answers.reason = candidate.note.substring(rindex, rend);
+							$scope.answers.connection = candidate.note.substring(cindex, cend);
+						}
+					}
+				}
+			}
+		});
+
+		$scope.spinnerOpts = {
+			lines: 		11,
+			length: 	12,
+			width: 		5,
+			radius: 	14,
+			corners: 	.5,
+			opacity: 	.05,
+			shadow: 	true,
+			color: 		['#73c92d', '#f7b518', '#C54E90']
+		};
+
+		$scope.sending = false;
+
+		$scope.tabs = [];
+		$scope.tabs[0] = true;
+		$scope.tabs[1] = false;
+		$scope.tabs[2] = false;
+		$scope.tabs[3] = false;
+		$scope.selected = 0;
+
+		$scope.next = function() {
+			//Simply set the next tab to be active, do not increment $scope.selected as it will be incremented by bootstrap-ui.
+			$scope.tabs[++$scope.selected] = true;
+		};
+
+		$scope.previous = function() {
+			//Simply set the previous tab to be active, do not increment $scope.selected as it will be incremented by bootstrap-ui.
+			$scope.tabs[--$scope.selected] = true;
+		};
+
+		/**
+		* Send candidate information to the backend.  The candidate's answers will be placed in the note
+		* field so an admin can look over them later.
+		*/
+		$scope.send = function() {
+			usSpinnerService.spin('candidate-form-spinner-1');
+			$scope.sending = true;
+			$scope.next();
+
+			var notes = "PLEASE DO NOT DELETE OR EDIT THIS SECTION:\n**********\n***Reason:\n" + $scope.answers.reason + "\n\n***Connections:\n" + $scope.answers.connection + "\n***************";
+
+			$http.post("candidate/setCandidate", {event_id : event._id, note : notes}).success(function() {
+				$scope.sending = false;
+				$scope.error = false;
+			}).error(function(error) {
+				$scope.sending = false;
+				$scope.error = true;
+			});
+		};
+
+		$scope.done = function(status) {
+			status = parseInt(status, 10);
+
+			if(status) {
+				$modalInstance.close(true);
+			} else {
+				$modalInstance.close(false);
+			}
 		};
 	}
 ]);
