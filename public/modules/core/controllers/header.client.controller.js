c2e09735c71f00bce1bfbb9aea83919f747357fb
Calvin Moore <c1moore@ufl.edu>
2015-07-05 17:59:03 -0400

    Added report a problem page to user dropdown menu.

--- public/modules/core/controllers/header.client.controller.js
+++ public/modules/core/controllers/header.client.controller.js
@@ -22,14 +22,35 @@ angular.module('core').controller('HeaderController', ['$scope', 'Authentication
 			$scope.isCollapsed = false;
 		});
 
+		/**
+		* Returns true if the menu item should not be on the menu based on the roles required for the page.
+		* 
+		* @param rolesNeeded (array, string) - if an array, the array should contain strings of the required
+		* roles.  If a string, a single comma (',') should be used to separate roles.  Accepted strings
+		* include:
+		*		- "admin"		- admin roles required
+		*		- "recruiter"	- recruiter role required
+		*		- "attendee"	- attendee role required
+		*		- "*"			- any of the above roles.  If specified, it MUST be the only specified role.
+		*/
 		$scope.hideLink = function(rolesNeeded) {
 			if (!$scope.authentication.user) {
 				return true;
 			}
-			else if (($filter('roles')($scope.authentication.user.roles,rolesNeeded)).length === 0) {
-				return true;
+
+			if(typeof rolesNeeded === 'string') {
+				if(rolesNeeded === '*') {
+					return false;
+				} else {
+					rolesNeeded = rolesNeeded.split(',');
+				}
+			} else if(rolesNeeded[0] === '*') {
+				return false;
 			}
-			else {
+			
+			if (($filter('roles')($scope.authentication.user.roles,rolesNeeded)).length === 0) {
+				return true;
+			} else {
 				return false;
 			}
 		};
