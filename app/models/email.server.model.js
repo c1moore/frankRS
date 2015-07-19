773f9592413e5b672e9e37963670aca54417afa8
Calvin Moore <c1moore@ufl.edu>
2015-07-19 13:48:43 -0400

    Added model for emails.

---
+++ app/models/email.server.model.js
@@ -0,0 +1,64 @@
+'user strict';
+
+/**
+* Module dependencies.
+*/
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema,
+	Types = Schema.Types,
+	idvalidator = require('mongoose-id-validator');
+
+/**
+* Validation function for required properties.
+*/
+var validateRequiredProperty = function(property) {
+	return (property && property.length);
+};
+
+/**
+* Email Schema
+*/
+var EmailSchema = new Schema({
+	to: {
+		type: 		String,
+		trim: 		true,
+		validate: 	[validateRequiredProperty, 'Please fill in your email'],
+		match: 		[/.+\@.+\..+/, 'Please fill a valid email address'],
+		required: 	true
+	},
+	from: {
+		type: 		String,
+		trim: 		true,
+		validate: 	[validateRequiredProperty, 'Please fill in your email'],
+		match: 		[/.+\@.+\..+/, 'Please fill a valid email address'],
+		required: 	true
+	},
+	subject: {
+		type: 		String,
+		trim: 		true,
+		default: 	''
+	},
+	message: {
+		type: 		String,
+		trim: 		true,
+		validate: 	[validateRequiredProperty, 'Message body required'],
+		required: 	true
+	},
+	read: {
+		type: 		Boolean,
+		default: 	false,
+		required: 	true
+	},
+	event_id: {
+		type: 		Types.ObjectId,
+		ref: 		'Event',
+		required: 	true
+	}
+});
+
+//Use idvalidator to make sure event_id references an existing event in the db.
+EmailSchema.plugin(idvalidator);
+
+mongoose.model('Email', EmailSchema);
+
+exports = EmailSchema;
