d23ca5d3b7e3ddb6b1a2b075e8dcb309fdd6d2f0
Calvin Moore <c1moore@ufl.edu>
2015-07-22 19:26:41 -0400

    Updated homepage icons.

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -24,11 +24,12 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 			// Temporary data for buttons
 			$scope.data = {
 				buttons: [
-					{name:"Admin Page", description:"A place where admins can fulfill their fantasies of being all-powerful.", link:'/#!/admin', roles:['admin'], image: "http://i.imgur.com/L12IQ3m.jpg"},
-					{name:"Control Room", description:"Send out your invitations and see your invitation stats.", link:'/#!/invite', roles:['recruiter','admin'], image: "http://frank.jou.ufl.edu/recruiters/Pictures/but3.jpg"},
-					{name:"Leaderboard", description:"See how your friends and competitors rank against you.", link:'/#!/leaderboard', roles:['recruiter','admin'], image: "http://frank.jou.ufl.edu/recruiters/Pictures/but4.jpg"},
-					{name:"frank Lounge", description:"Take a look at what people are saying about the events you are attending and weigh in on the chatter.", link:'/#!/franklounge', roles:['admin', 'recruiter', 'attendee'], image: "http://i.imgur.com/9UYHlAy.jpg"},
-					{name:"Recruiter Registration", description:"Think you got what it takes to be a recruiter for frank?  Then sign up here.  Warning: we only accept the best.", link:'/#!/events', roles:['recruiter', 'attendee'], titleId:"recruiter-request-button-title", image: "http://i.imgur.com/CSPFqYn.jpg"}
+					{name:"Admin Page", description:"A place where admins can fulfill their fantasies of being all-powerful.", link:'/#!/admin', roles:['admin'], image: "/modules/core/img/icons/admin.png"},
+					{name:"Control Room", description:"Send out your invitations and see your invitation stats.", link:'/#!/invite', roles:['recruiter','admin'], image: "/modules/core/img/icons/invites.png"},
+					{name:"Leaderboard", description:"See how your friends and competitors rank against you.", link:'/#!/leaderboard', roles:['recruiter','admin'], image: "/modules/core/img/icons/leaderboard.png"},
+					{name:"frank Lounge", description:"Take a look at what people are saying about the events you are attending and weigh in on the chatter.", link:'/#!/franklounge', roles:['admin', 'recruiter', 'attendee'], image: "/modules/core/img/icons/frank_lounge.png"},
+					{name:"Recruiter Registration", description:"Think you got what it takes to be a recruiter for frank?  Then sign up here.  Warning: we only accept the best.", link:'/#!/events', roles:['recruiter', 'attendee'], titleId:"recruiter-request-button-title", image: "/modules/core/img/icons/recruiter_registration.png"},
+					{name:"Register", description:"Register to attend frank 2016.", link:'https://frank2016.eventbrite.com/', roles:['recruiter', 'admin'], image: "/modules/core/img/icons/register.png"}
 				]
 				/*,
 				comments: ["Comment 1", "Comment 2"]
