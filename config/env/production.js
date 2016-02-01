eee6933d122b5b6f3ca2c9a297813f6daef13f37
Calvin Moore <c1moore@ufl.edu>
2016-02-01 16:24:38 -0500

    Updated production rules.

--- config/env/production.js
+++ config/env/production.js
@@ -44,7 +44,10 @@ module.exports = {
 				'public/lib/angular-dialog-service/example/js/dialogs.min.js',
 				'public/lib/spin.js/spin.js',
 				'public/lib/spin.js/jquery.spin.js',
-				'public/lib/angular-spinner/angular-spinner.min.js'
+				'public/lib/angular-spinner/angular-spinner.min.js',
+				'public/lib/angular-drag-and-drop-lists/angular-drag-and-drop-lists.min.js',
+				'public/lib/angular-local-storage/dist/angular-local-storage.min.js',
+				'public/lib/async/dist/async.min.js'
 			]
 		},
 		css: 'public/dist/application.min.css',
